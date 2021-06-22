452a80de90ae427f8adbe8a44124bce8a2fb9287
maheeeeennn <82027396+maheeeeennn@users.noreply.github.com>
2021-06-22 16:09:02 +0500

    2nd

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,31 @@ var Product = require('../models/ProductModel');
 /* GET home page. */
 router.get('/',async  function(req, res, next) {
   products = await Product.find();
-  // console.log(products);
+  console.log(req.session.user);
   res.render('index',{ products });
 });
-
+router.get('/add',async  function(req, res, next) {
+  res.render('add');
+});
+router.get('/cart',async  function(req, res, next) {
+  cart = req.cookies.cart;
+  if(!cart)
+  cart = [];
+  res.render('cart',{cart});
+});
+router.get('/cart/:id',async  function(req, res, next) {
+  product = await Product.findById(req.params.id);
+  console.log(product);
+  let cart = [];
+  if(req.cookies.cart) cart =  req.cookies.cart;
+  cart.push(product);
+  res.cookie('cart',cart);
+  res.redirect('/');
+});
+router.post('/add',async  function(req, res, next) {
+  product = new Product(req.body);
+  console.log(product);
+  await product.save();
+  res.redirect('/');
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
 const Users = require("../models/UsersModel");
+
 /* GET users listing. */
-router.get('/',async function(req, res, next) {
-  res.send('respond with a resource');
-  users =await Users.find();
-  console.log(users);
+router.get('/signup',async function(req, res, next) {
+  console.log(req.Session.user);
+  res.render('signup');
+});
+router.get('/login',async function(req, res, next) {
+  res.render('login');
+});
+router.post('/login',async function(req, res, next) {
+  let user = await Users.findOne({
+    email: req.body.email,
+    password : req.body.password
+  });
+  if(!user)
+  {
+    res.redirect('/users/login');
+  }
+  console.log(user);
+  req.session.user = user;
+  res.redirect('/');
+});
+router.post('/signup',async function(req, res, next) {
+  user = new Users(req.body);
+  await user.save();
+  res.redirect('/users/login');
 });
 
 module.exports = router;
